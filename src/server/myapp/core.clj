38b81ed224804df13aa6d01492f51c436a0ec49e
Adam Moore <nerdfunk@gmail.com>
2018-05-09 13:38:05 +0900

    test txt

--- src/server/myapp/core.clj
+++ src/server/myapp/core.clj
@@ -15,24 +15,5 @@
             [taoensso.timbre :as timbre]))
 
 (defn -main [& args]
-  (cond
-    (some #{"init"} args)
-    (do
-      (timbre/info "Initializing DB")
-      (mount/start (mount/except [#'n/nrepl
-                                  #'s/server
-                                  #'h/wrapped-handler]))
-      (db/migrate)
-      (let [console (. System console)
-            username (do (print "Admin Username: ") (flush) (read-line))
-            pwd (String/valueOf (.readPassword (System/console) "Password:" nil))]
-        (db/create-user! username pwd))
-      (System/exit 0))
-    (some #{"migrate" "rollback"} args)
-    (do
-      (mount/start)
-      (db/rollback)
-      (System/exit 0))
-    :else
-    (do (timbre/info "M O U N T I N G")
-        (mount/start))))
+  (do (timbre/info "M O U N T I N G")
+      (mount/start)))
